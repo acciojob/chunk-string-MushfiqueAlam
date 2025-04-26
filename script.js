03547670abed44e9a38a4f8db50961671deb70ba
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-04-26 16:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ function stringChop(str, size) {
 	size=Number(size);
 	let s="";
 	let arr=[];
-	for(let i=0;i<str.length;i++){
-		s+=str[i];
-		if(s.length===size){
-			arr.push(s);
-			s="";
-		}
+	// for(let i=0;i<str.length;i++){
+	// 	s+=str[i];
+	// 	if(s.length===size){
+	// 		arr.push(s);
+	// 		s="";
+	// 	}
 	}
-	if(s.length>0)
-		arr.push(s);
+	// if(s.length>0)
+	// 	arr.push(s);
+
+	for(let i=0;i<str.length;i=i+size){
+		let subStr=str.slice(i,i+size);
+		arr.push(subStr);
+	}
 	return arr;
 	
 }
-
 // Do not change the code below
+
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
